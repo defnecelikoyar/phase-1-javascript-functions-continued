5125d75ecc244819f58484aa0c1242bfe67dc198
Defne Celikoyar <defne.celikoyar@gmail.com>
2021-11-23 13:40:13 -0500

    commit tree 6c9ba992943bd9835b356556ac1d265de4058441

--- index.js
+++ index.js
@@ -6,8 +6,22 @@ let mondayWork = function(activity="go to the office") {
   return `This Monday, I will ${activity}.`
 }
 
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
-    return `You are ${style}${adjective}${style}!`
+
+
+function wrapAdjective(flair="*") {
+  return function(par1=special) {
+    return `You are ${flair}${par1}${flair}!`
   }
 }
+
+
+//Implement a function called wrapAdjective:
+//It should return a function
+//////////This "inner" function should:
+//////////take a single parameter that should default to "special". Name it however you wish.
+/////////return a String of the form "You are ..." where ... should be the adjective this function received
+/////////wrapped in visual flair
+//It should take as parameter a String that will be used to create visual flair
+//You may call the parameter whatever you like, but its default value should be "*"
+//Call example: const encouragingPromptFunction = wrapAdjective("!!!")
+
